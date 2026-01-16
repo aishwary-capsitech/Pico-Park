e2a72f3f28b204799319f2f8eeaed790ac458103
aishwary-capsitech <aishwary.meher@capsitech.com>
2026-01-16 18:03:43 +0530

    updated player prefab

--- Assets/Scripts/PlayerSwipeCrouch.cs
+++ Assets/Scripts/PlayerSwipeCrouch.cs
@@ -53,3 +53,112 @@ public class PlayerSwipeCrouch : NetworkBehaviour
         transform.localScale = scale;
     }
 }
+
+
+
+//using Fusion;
+
+//using UnityEngine;
+
+//[RequireComponent(typeof(CapsuleCollider2D))]
+
+//public class PlayerSwipeCrouch : NetworkBehaviour
+
+//{
+
+//    public float standingHeight = 1.8f;
+
+//    public float crouchHeight = 1.0f;
+
+//    private CapsuleCollider2D col;
+
+//    [Networked] private NetworkBool IsCrouching { get; set; }
+
+//    public override void Spawned()
+
+//    {
+
+//        col = GetComponent<CapsuleCollider2D>();
+
+//        if (Object.HasStateAuthority)
+
+//            IsCrouching = false;
+
+//        ApplyCollider();
+
+//    }
+
+//    public void StartCrouch()
+
+//    {
+
+//        if (!Object.HasInputAuthority) return;
+
+//        RPC_SetCrouch(true);
+
+//    }
+
+//    public void StopCrouch()
+
+//    {
+
+//        if (!Object.HasInputAuthority) return;
+
+//        if (!CanStand()) return;
+
+//        RPC_SetCrouch(false);
+
+//    }
+
+//    [Rpc(RpcSources.InputAuthority, RpcTargets.StateAuthority)]
+
+//    private void RPC_SetCrouch(NetworkBool crouch)
+
+//    {
+
+//        IsCrouching = crouch;
+
+//        ApplyCollider();
+
+//    }
+
+//    private void ApplyCollider()
+
+//    {
+
+//        if (col == null) return;
+
+//        col.size = new Vector2(col.size.x, IsCrouching ? crouchHeight : standingHeight);
+
+//        col.offset = new Vector2(col.offset.x, col.size.y / 2f);
+
+//    }
+
+//    private bool CanStand()
+
+//    {
+
+//        if (col == null) return false;
+
+//        Vector2 originalSize = col.size;
+
+//        Vector2 originalOffset = col.offset;
+
+//        col.size = new Vector2(col.size.x, standingHeight);
+
+//        col.offset = new Vector2(col.offset.x, standingHeight / 2f);
+
+//        RaycastHit2D[] hits = new RaycastHit2D[1];
+
+//        int hitCount = col.Cast(Vector2.up, hits, 0f);
+
+//        col.size = originalSize;
+
+//        col.offset = originalOffset;
+
+//        return hitCount == 0;
+
+//    }
+
+//}
+
